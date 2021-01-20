a646d4a87157aafaa615b4d89b6e31644d65247c
eli <eli.pond21@mail.ru>
2021-01-20 21:52:27 +0300

    start_project

---
+++ js/jquery.eislideshow.js
@@ -0,0 +1,21 @@
+(function (window, $, undefined) {
+
+    //    //бургер
+    //
+    $('.menu-burger__header').click(function () {
+        $('.menu-burger__header').toggleClass('open-menu');
+        $('.header__nav').toggleClass('open-menu');
+        $('.fon-blac').toggleClass('open-menu');
+    });
+
+    /*
+     * smartresize: debounced resize event for jQuery
+     *
+     * latest version and complete README available on Github:
+     * https://github.com/louisremi/jquery.smartresize.js
+     *
+     * Copyright 2011 @louis_remi
+     * Licensed under the MIT license.
+     */
+
+})(window, jQuery);
